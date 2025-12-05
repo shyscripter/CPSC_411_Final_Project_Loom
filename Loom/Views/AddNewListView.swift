08ca2fe36cfc6e1ed64f5b1aaba26f516c534650
Aaron Haight <shyguydude01298@gmail.com>
2025-12-04 20:43:03 -0800

    Completed part 2 of the youtube tutorial

--- Loom/Views/AddNewListView.swift
+++ Loom/Views/AddNewListView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct AddNewListView: View {
     
+    @Environment(\.dismiss) private var dismiss
     @State private var name: String = ""
+    @State private var selectedColor: Color = .yellow
+    
+    let onSave: (String, UIColor) -> Void
+    
+    private var isFormValid: Bool {
+        !name.isEmpty
+    }
     
     var body: some View {
         
         VStack {
             Image(systemName: "line.3.horizontal.circle.fill")
-                .foregroundColor(.green)
+                .foregroundColor(selectedColor)
                 .font(.system(size: 100))
+            
             TextField("List Name", text: $name)
                 .multilineTextAlignment(.center)
                 .textFieldStyle(.roundedBorder)
@@ -24,11 +33,38 @@ struct AddNewListView: View {
         .padding(30)
         .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
         
+        ColorPickerView(selectedColor: $selectedColor)
+        
         Spacer()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    // FIXED LINE 32 BELOW:
+                    Text("New List")
+                        .font(.headline)
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Done") {
+                        
+                        // save the list
+                        onSave(name, UIColor(selectedColor))
+                        
+                        dismiss()
+                    }.disabled(isFormValid)
+                }
+            }
     }
-    
 }
 
 #Preview {
-    AddNewListView()
+    NavigationView {
+        AddNewListView(onSave: { (_, _) in })
+    }
 }
---
+++ Loom/Views/HomeView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Loom
+//
+//  Created by Aaron Haight on 11/24/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var isPresented: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Hello world")
+                
+                
+                Button {
+                    isPresented = true
+                } label: {
+                    Text("Add List")
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .font(.headline)
+                }.padding()
+            }.sheet(isPresented: $isPresented) {
+                NavigationView {
+                    AddNewListView { name, color in
+                        // save the list to the database
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+    
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
