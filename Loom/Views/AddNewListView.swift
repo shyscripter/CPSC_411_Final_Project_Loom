7d6631bad023e60fdfcd728d389c60d2334207f7
Aaron Haight <shyguydude01298@gmail.com>
2025-12-05 10:28:57 -0800

    Fixed UI issue from mistake in part 2

--- Loom/Views/AddNewListView.swift
+++ Loom/Views/AddNewListView.swift
@@ -22,43 +22,44 @@ struct AddNewListView: View {
     var body: some View {
         
         VStack {
-            Image(systemName: "line.3.horizontal.circle.fill")
-                .foregroundColor(selectedColor)
-                .font(.system(size: 100))
+            VStack {
+                Image(systemName: "line.3.horizontal.circle.fill")
+                    .foregroundColor(selectedColor)
+                    .font(.system(size: 100))
+                
+                TextField("List Name", text: $name)
+                    .multilineTextAlignment(.center)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding(30)
+            .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
             
-            TextField("List Name", text: $name)
-                .multilineTextAlignment(.center)
-                .textFieldStyle(.roundedBorder)
-        }
-        .padding(30)
-        .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
-        
-        ColorPickerView(selectedColor: $selectedColor)
-        
-        Spacer()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("New List")
-                        .font(.headline)
-                }
-                
-                ToolbarItem(placement: .topBarLeading) {
-                    Button("Close") {
-                        dismiss()
-                    }
-                }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Done") {
-                        
-                        // save the list
-                        onSave(name, UIColor(selectedColor))
-                        
-                        dismiss()
-                    }.disabled(isFormValid)
+            ColorPickerView(selectedColor: $selectedColor)
+            
+            Spacer()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("New List")
+                    .font(.headline)
+            }
+            
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    dismiss()
                 }
             }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Done") {
+                    
+                    // save the list
+                    onSave(name, UIColor(selectedColor))
+                    
+                    dismiss()
+                }.disabled(isFormValid)
+            }
+        }
     }
 }
 
