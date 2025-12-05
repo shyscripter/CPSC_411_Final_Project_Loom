a26d978dcc335b8ac1682521ca71fd6c73dd344f
Aaron Haight <shyguydude01298@gmail.com>
2025-12-05 12:42:58 -0800

    Completed part 10 of tutorial

---
+++ Loom/Views/SelectListView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectListView.swift
+//  Loom
+//
+//  Created by Aaron Haight on 12/5/25.
+//
+
+import SwiftUI
+
+struct SelectListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectListView()
+}
