c79493fdb4137e3419ba17e130255897b777b330
Aaron Haight <shyguydude01298@gmail.com>
2025-12-04 19:40:49 -0800

    Created CoreDataProvider file

---
+++ Loom/Providers/CoreDataProvider.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataProvider.swift
+//  Loom
+//
+//  Created by Aaron Haight on 12/4/25.
+//
+
+import Foundation
+import CoreData
+
+// Class is responsible for setting up the CoreData upon app loading
+class CoreDataProvider {
+    
+    // We only want one instance of this provider
+    static let shared = CoreDataProvider()
+    let persistentContainer: NSPersistentContainer
+    
+    // Private constructor to make sure there's only one provider
+    private init() {
+        
+        // Create a persistent container, app crashes if this model doesn't exist
+        persistentContainer = NSPersistentContainer(name: "ReminderModel")
+        persistentContainer.loadPersistentStores() { desc, error in
+            if let error {
+                fatalError("Error initializing ReminderModel \(error)")
+            }
+            
+        }
+        
+    }
+    
+}
