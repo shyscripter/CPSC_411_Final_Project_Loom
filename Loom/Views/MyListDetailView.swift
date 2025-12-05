d1c4a6752af412da460a351bde94f9d4d2b00333
Aaron Haight <shyguydude01298@gmail.com>
2025-12-05 11:34:46 -0800

    Compelted part 6 of tutorial

---
+++ Loom/Views/MyListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListDetailView.swift
+//  Loom
+//
+//  Created by Aaron Haight on 12/5/25.
+//
+
+import SwiftUI
+
+struct MyListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListDetailView()
+}
