fd40069db3242c34991739449d39cb95fd3ddeba
Aaron Haight <shyguydude01298@gmail.com>
2025-12-05 11:11:28 -0800

    Completed part 4 of tutorial

---
+++ Loom/PreviewContent/PreviewData.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewData.swift
+//  Loom
+//
+//  Created by Aaron Haight on 12/5/25.
+//
+
+import Foundation
---
+++ Loom/Views/MyListsView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListsView.swift
+//  Loom
+//
+//  Created by Aaron Haight on 12/5/25.
+//
+
+import SwiftUI
+
+struct MyListsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListsView()
+}
