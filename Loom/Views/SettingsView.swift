9ae162fd31250921f772188413986de1ed6de0df
Aaron Haight <shyguydude01298@gmail.com>
2025-12-11 17:15:03 -0800

    Created new file to view app settings in their own page

---
+++ Loom/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Loom
+//
+//  Created by Aaron Haight on 12/11/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
